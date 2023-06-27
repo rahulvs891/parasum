3072b2db4e8bc3a6478678126ac8420a5fa735f2
Rahul v s <rahulvs037@gmail.com>
2023-06-27 12:40:44 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './pages/Homepage';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
-      </BrowserRouter>
+      <Homepage/>
     </div>
   );
 }
